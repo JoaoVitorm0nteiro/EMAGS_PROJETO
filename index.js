43b70c25541aa9982d8a6600b1d2b61bec306078
JoaoVitorm0nteiro <jv147pao@gmail.com>
2023-05-27 22:40:55 -0300

    add: register screen

--- index.js
+++ index.js
@@ -9,8 +9,22 @@ app.use(express.static('public'));
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
+
+//PAGINA DE LOGIN
 app.get('/login', (req,res)=>{
-    res.render('login')
+    res.render('login',{
+        title:'Login',
+        style:'login.css'
+
+    });
+});
+
+//PAGINA DE REGISTRO
+app.get('/register', (req, res)=>{
+   res.render('register',{
+    title:'Register',
+    style:'register.css'
+   }); 
 });
 
 app.listen(port, ()=>{
